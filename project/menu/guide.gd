2b788a6950b00225ff5cd52f3fef199b021f0655
fetasty <fetasty@outlook.com>
2025-03-31 22:13:41 +0800

    feat: Part of guide

---
+++ project/menu/guide.gd
@@ -0,0 +1,25 @@
+extends Control
+
+
+var _guide_player_jump_speed: float = -170.0
+var _guide_player_gravity: float = 900.0
+var _guide_player_jump_y: float = 150.5
+
+var _guide_player_velocity: float = 0.0
+var _guide_player_jump_time: float = 0.0
+
+@onready var guide_player: TextureRect = $Guide1/GuidePlayer
+
+
+func _ready() -> void:
+	_guide_player_jump_y = guide_player.position.y
+
+func _process(delta: float) -> void:
+	if _guide_player_jump_time > 0:
+		_guide_player_jump_time -= delta
+		_guide_player_velocity = _guide_player_jump_speed
+	else:
+		_guide_player_velocity += delta * _guide_player_gravity
+	guide_player.position.y += _guide_player_velocity * delta
+	if guide_player.position.y > _guide_player_jump_y:
+		_guide_player_jump_time = 0.2
